73892ed94eeea55bdb33f74b98c4a044575c0a50
Abhinav Prakash <abhinav.prakash1@gmail.com>
2023-02-05 12:36:25 +0530

    Created models and added rest framework lib

--- vehicle_details/vehicle_api/models.py
+++ vehicle_details/vehicle_api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class NewVehicle(models.Model):
+    make = models.CharField(max_length=60)
+    model = models.CharField(max_length=60)
+    year = models.IntegerField(blank=True)
+    price = models.IntegerField(blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.make} {self.model}"
--- vehicle_details/vehicle_details/settings.py
+++ vehicle_details/vehicle_details/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Added frameworks
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
